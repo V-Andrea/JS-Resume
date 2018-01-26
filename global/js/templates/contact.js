2a9adeea999f3adde616e6b39a10acdbe4d9b247
T-Andrea <t.andrea.phoner@gmail.com>
2018-01-26 13:30:26 +0100

    Contact info links opening in new page

--- global/js/templates/contact.js
+++ global/js/templates/contact.js
@@ -2,13 +2,13 @@ export const addContacts = data =>
     `
     <ul>
       <li>
-        <a class="class-link" href="${data.contacts.email}">E-mail</a>
+        <a class="class-link" target="_blank" href="mailto:${data.contacts.email}">E-mail</a>
       </li>
       <li>
-        <a class="class-link" href="${data.contacts.github}">Github</a>
+        <a class="class-link" target="_blank" href="${data.contacts.github}">Github</a>
       </li>
       <li>
-        <a class="class-link" href="${data.contacts.linkedin}">LinkedIn</a>
+        <a class="class-link" target="_blank" href="${data.contacts.linkedin}">LinkedIn</a>
       </li>
     </ul>
     `
--- page/resume.js
+++ page/resume.js
@@ -374,13 +374,13 @@ const addStatement = data => `
 const addContacts = data => `
     <ul>
       <li>
-        <a class="class-link" href="${data.contacts.email}">E-mail</a>
+        <a class="class-link" target="_blank" href="mailto:${data.contacts.email}">E-mail</a>
       </li>
       <li>
-        <a class="class-link" href="${data.contacts.github}">Github</a>
+        <a class="class-link" target="_blank" href="${data.contacts.github}">Github</a>
       </li>
       <li>
-        <a class="class-link" href="${data.contacts.linkedin}">LinkedIn</a>
+        <a class="class-link" target="_blank" href="${data.contacts.linkedin}">LinkedIn</a>
       </li>
     </ul>
     `;
