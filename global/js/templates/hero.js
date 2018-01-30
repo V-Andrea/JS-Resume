b9723730eeed98e680c97c577a7a63454f489f1f
T-Andrea <t.andrea.phoner@gmail.com>
2018-01-30 23:12:18 +0100

    Basic css styles added and hero styled

--- global/js/templates/hero.js
+++ global/js/templates/hero.js
@@ -1,8 +1,12 @@
 export const createHero = data =>
     `
-    <h1 class="h1">${data.firstName} ${data.lastName}</h1>
-    <p>Hi, I'm ${data.firstName},</p>
-    <strong>${data.role},</strong>
-    <p>${data.profile}</p>
-    <p>${data.welcomeMessage}</p>
+    <div class="title">
+      <h1 class="h1">${data.firstName} ${data.lastName}</h1>
+    </div>
+    <div class="greeting">
+      <p>Hi, I'm ${data.firstName},</p>
+      <strong>${data.role},</strong>
+      <p>${data.profile}</p>
+      <p>${data.welcomeMessage}</p>
+    </div>
     `
