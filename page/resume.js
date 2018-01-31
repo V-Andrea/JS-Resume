e5c48e7eb946aac0dbb496b2d61554a88f59d210
T-Andrea <t.andrea.phoner@gmail.com>
2018-01-31 10:44:46 +0100

    Added circular chart styles

--- page/resume.js
+++ page/resume.js
@@ -259,11 +259,15 @@ let view = {
 
 "use strict";
 const createHero = data => `
-    <h1 class="h1">${data.firstName} ${data.lastName}</h1>
-    <p>Hi, I'm ${data.firstName},</p>
-    <strong>${data.role},</strong>
-    <p>${data.profile}</p>
-    <p>${data.welcomeMessage}</p>
+    <div class="title">
+      <h1 class="h1">${data.firstName} ${data.lastName}</h1>
+    </div>
+    <div class="greeting">
+      <p>Hi, I'm ${data.firstName},</p>
+      <strong>${data.role},</strong>
+      <p>${data.profile}</p>
+      <p>${data.welcomeMessage}</p>
+    </div>
     `;
 /* harmony export (immutable) */ __webpack_exports__["a"] = createHero;
 
