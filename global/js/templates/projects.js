fbb7937621d3b719b6b198b92d9e72d22543b312
T-Andrea <t.andrea.phoner@gmail.com>
2018-02-20 20:48:56 +0100

    Adjusted styles

--- global/js/templates/projects.js
+++ global/js/templates/projects.js
@@ -1,19 +1,21 @@
 export const addProjects = data =>
   `
-  <h2 class="text-center">Projects</h2>
-  <ul class="container">
-    ${data.projects.map(project => `
-    <li class="card">
-      <img class="card-img" src="${project.image}">
-      <div class="card-body">
-        <h6 class="card-title">${project.title}</h6>
-        <div class="date-text">${project.dates}</div>
-        <p>${project.description}</p>
-      </div>
-      <div class="card-footer">
-        <a target="_blank" href="Github">${project.github}</a>
-      </div>
-    </li>
-    `).join('')}
-  </ul>
+  <div class="card-group">
+    <h2 class="text-center">Projects</h2>
+    <ul class="container">
+      ${data.projects.map(project => `
+      <li class="card">
+        <img class="card-img" src="${project.image}">
+        <div class="card-body">
+          <h6 class="card-title">${project.title}</h6>
+          <div class="date-text">${project.dates}</div>
+          <p>${project.description}</p>
+        </div>
+        <div class="card-footer">
+          <a target="_blank" href="Github">${project.github}</a>
+        </div>
+      </li>
+      `).join('')}
+    </ul>
+  </div>
   `
